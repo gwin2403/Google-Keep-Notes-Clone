b85e6455d27691f3d78f6b41f0f29a0255b57242
logeshwars <stylishstarlogesh15@gmail.com>
2022-09-29 00:28:24 +0530

    drag drop added

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Notes from "./Notes";
 import "./Task.css";
 export default function Task() {
+  const [arr, setArr] = useState([
+    { data: "this a text" },
+    { data: "the data " },
+    { data: "make dish today" },
+    { data: "go to site" },
+    { data: "finish project today" },
+    { data: "start new project" },
+  ]);
+  const [dragged, setDragged] = useState();
+  const [dragOver, setDragOver] = useState();
   return (
     <div className="Task-input">
       <div className="Task-01">
@@ -13,12 +23,19 @@ export default function Task() {
         </div>
       </div>
       <div className="NotesConatainer">
-        <Notes />
-        <Notes />
-        <Notes />
-        <Notes />
-        <Notes />
-        <Notes />
+        {arr.map((val, i) => (
+          <Notes
+            value={val}
+            id={i}
+            key={i}
+            dragged={dragged}
+            setDragged={setDragged}
+            dragOver={dragOver}
+            setDragOver={setDragOver}
+            setArr={setArr}
+            arr={arr}
+          />
+        ))}
       </div>
     </div>
   );
